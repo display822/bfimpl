3aa0ff1054e1bfe8d16d7be0f66b50032a3e4680
iain <iain@iainen.com>
2020-12-21 23:34:32 +0800

    fix list status NA

--- controllers/oa_expense.go
+++ controllers/oa_expense.go
@@ -103,8 +103,8 @@ func (e *ExpenseController) List() {
 		}
 		if len(s) == 0 {
 			services.Slave().Debug().Raw("select w.entity_id from workflows w,workflow_nodes wn where w.id = "+
-				"wn.workflow_id and w.workflow_definition_id = ? and operator_id = ? and status <> 'NA'"+
-				" and wn.node_seq != 1", services.GetFlowDefID(services.Expense), userID).Scan(&ids)
+				"wn.workflow_id and w.workflow_definition_id = ? and operator_id = ? and status <> ?"+
+				" and wn.node_seq != 1", services.GetFlowDefID(services.Expense), userID, "NA").Scan(&ids)
 		} else {
 			services.Slave().Debug().Raw("select w.entity_id from workflows w,workflow_nodes wn where w.id = "+
 				"wn.workflow_id and w.workflow_definition_id = ? and operator_id = ? and wn.status in (?)"+
