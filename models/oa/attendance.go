26b72c6207e6c132a68547d40a4d7bfaf84d85c8
chenlie <13720335094@163.com>
2021-01-04 14:56:26 +0800

    test

--- models/oa/attendance.go
+++ models/oa/attendance.go
@@ -99,7 +99,7 @@ type AttendanceSimple struct {
 }
 
 func (v AttendanceSimple) String(now string) string {
-	return fmt.Sprintf("('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%f,%f)", now, v.Dept, v.Name,
+	return fmt.Sprintf("('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%f,%f)", now, v.Dept, v.Name,
 		v.AttendanceDate.String(), v.CheckIn.String(), v.CheckOut.String(), v.InStatus, v.OutStatus,
 		v.InResult, v.OutResult, v.LeaveId, v.Overtime, v.Shift)
 }
